bf167e5932653d075f242ab214debfaebde7e0ed
1sk4ndar <134580370+1sk4ndar@users.noreply.github.com>
2023-05-24 23:10:20 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,77 @@
+
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+    
+     <ul className='menu'>
+      <li className='sub'>Tortas</li>
+      <ul>
+      <input type='checkbox'></input><li className='menu_items'>Bolillo</li>
+      <input type='checkbox'></input><li  className='menu_items'>Telera</li>
+      <input type='checkbox'></input><li className='menu_items'>Chile molido</li>
+      <input type='checkbox'></input><li className='menu_items'>Chile jalapeño</li>
+      <input type='checkbox'></input><li className='menu_items'>Con todo</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin chile</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin cebolla</li>
+      </ul>
+      <ul>
+      <input type='checkbox'></input><li className='menu_items'>Sin jitomate</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin lechuga</li>
+      <input type='checkbox'></input><li className='menu_items'>Especial de pierna</li>
+      <input type='checkbox'></input><li className='menu_items'>Especial de pollo</li>
+      <input type='checkbox'></input><li className='menu_items'>Pierna</li>
+      <input type='checkbox'></input><li className='menu_items'>Pollo</li>
+      <input type='checkbox'></input><li className='menu_items'>Jamon con queso</li>
+      </ul>
+      <li className='sub'>Sandwich</li>
+      <ul>
+      <input type='checkbox'></input><li className='menu_items'>Pan blanco</li>
+      <input type='checkbox'></input><li  className='menu_items'>Pan integral</li>
+      <input type='checkbox'></input><li className='menu_items'>Chile molido</li>
+      <input type='checkbox'></input><li className='menu_items'>Chile jalapeño</li>
+      <input type='checkbox'></input><li className='menu_items'>Con todo</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin chile</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin cebolla</li>
+      </ul>
+      <ul>
+      <input type='checkbox'></input><li className='menu_items'>Sin jitomate</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin lechuga</li>
+      <input type='checkbox'></input><li className='menu_items'>Especial de pierna</li>
+      <input type='checkbox'></input><li className='menu_items'>Especial de pollo</li>
+      <input type='checkbox'></input><li className='menu_items'>Pierna</li>
+      <input type='checkbox'></input><li className='menu_items'>Pollo</li>
+      <input type='checkbox'></input><li className='menu_items'>Jamon con queso</li>
+      </ul>
+      <li className='sub'>Burritos</li>
+      <ul>
+      <input type='checkbox'></input><li className='menu_items'>Pierna</li>
+      <input type='checkbox'></input><li  className='menu_items'>Pollo</li>
+      <input type='checkbox'></input><li className='menu_items'>Verdura encima</li>
+      <input type='checkbox'></input><li className='menu_items'>Verdura al lado</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin cebolla</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin salsa picada</li>
+      <input type='checkbox'></input><li className='menu_items'>Sin lechuga</li>
+      <input type='checkbox'></input><li className='menu_items'>Con todo</li>
+      </ul>
+      <li className='sub'>Sincronizada</li>
+      <li className='sub'>Quesadillas</li>
+      <li className='sub'>Tostadas</li>
+      <li className='sub'>Bionicos</li>
+      <li className='sub'>Yogurt</li>
+      <li className='sub'>Jugo de naranja</li>
+      <li className='sub'>Jugo de zanahoria</li>
+      <li className='sub'>Jugo de betavel</li>
+      <li className='sub'>Jugo verde</li>
+      <li className='sub'>Jugo de piña</li>
+      <li className='sub'>Jugo combinado</li>
+      <li className='sub'>Jugo de toronja</li>
+      <li className='sub'>Chocomil</li>
+      <li className='sub'>Licuado</li>
+    
+      </ul> 
+    </div>
+  );
+}
+
+export default App;
